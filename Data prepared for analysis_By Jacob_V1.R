7c4c68dd9e836f8486a574e5792e007be5ff2da2
Sonali <sonali.syngal17@imperial.ac.u>
2017-09-26 15:45:13 +0100

    Data prep

---
+++ Data prepared for analysis_By Jacob_V1.R
@@ -0,0 +1,18 @@
+library(dplyr)
+df <- read.csv("database.csv", header = TRUE)
+
+names_to_drop <- c('Agency.Code', 'Record.ID', 'Agency.Name', 'Record.Source', 
+                   'Perpetrator.Ethnicity')
+
+
+murder.num <- df %>%
+  filter(Crime.Type == "Murder or Manslaughter") %>%
+  select(-one_of(names_to_drop)) %>%
+  filter(Victim.Age > 0 & Victim.Age < 99) %>%
+  filter(!(Perpetrator.Race == 'Unknown' & Crime.Solved == 'Yes')) %>%
+  mutate(Victim.Counts = Victim.Count + 1, Perpetrator.Counts = Perpetrator.Count +1) %>%
+  select(-one_of('Victim.Count', 'Perpetrator.Count'))
+
+summary(murder.num)
+names(murder.num)
+head(murder.num)
