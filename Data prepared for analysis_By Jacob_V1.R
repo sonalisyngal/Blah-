cab9a63ed47d58912c8545dc8f74079798cf8df3
jjankel <jjankel123@gmail.com>
2017-09-27 16:06:29 +0100

    Updated code for getting df
    Cali df now included

--- Data prepared for analysis_By Jacob_V1.R
+++ Data prepared for analysis_By Jacob_V1.R
@@ -1,9 +1,10 @@
 library(dplyr)
 df <- read.csv("database.csv", header = TRUE)
 
+
 names_to_drop <- c('Agency.Code', 'Record.ID', 'Agency.Name', 'Record.Source', 
                    'Perpetrator.Ethnicity')
-
+  
 
 murder.num <- df %>%
   filter(Crime.Type == "Murder or Manslaughter") %>%
@@ -17,4 +18,24 @@ summary(murder.num)
 names(murder.num)
 head(murder.num)
 
+cali.cities <- murder.num %>% 
+  group_by(State, City) %>%
+  filter(State == 'California') %>%
+  summarise(n = n()) %>%
+  filter( n > 32)
+
+cali.df <- murder.num %>% 
+  group_by(State, City) %>%
+  filter(State == 'California') %>%
+  ungroup() %>%
+  group_by(City) %>%
+  mutate(n = n()) %>%
+  filter(n>32) %>%
+  ungroup()
+
+cali.df  
+
+
+num.cities.approved <- n_distinct(cali.cities$City) #Equals to 51 cities in california with 32+ crimes
+cali.cities
 
